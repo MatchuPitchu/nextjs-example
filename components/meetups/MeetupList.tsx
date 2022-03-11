dfa70591644fea8a44c8ad88f95ed825794cae11
MatchuPitchu <michael.flohr@posteo.de>
2022-03-11 14:26:17 +0100

    basic setup project branch

---
+++ components/meetups/MeetupList.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from 'next';
+import MeetupItem from './MeetupItem';
+import classes from './MeetupList.module.css';
+
+type Meetup = {
+  id: string;
+  image: string;
+  title: string;
+  address: string;
+};
+
+interface Props {
+  meetups: Meetup[];
+}
+
+const MeetupList: NextPage<Props> = (props) => {
+  return (
+    <ul className={classes.list}>
+      {props.meetups.map((meetup) => (
+        <MeetupItem
+          key={meetup.id}
+          id={meetup.id}
+          image={meetup.image}
+          title={meetup.title}
+          address={meetup.address}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default MeetupList;
